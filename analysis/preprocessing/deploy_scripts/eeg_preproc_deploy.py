d4fe583d5f5095f840afa9fa40ee1b492b1a5c9f
Bauer Hoss <cccbauer@FACKHG6GKJG32.tld>
2026-05-02 15:57:18 -0400

    Fix utils import path in eeg_preproc_deploy.py

--- analysis/preprocessing/deploy_scripts/eeg_preproc_deploy.py
+++ analysis/preprocessing/deploy_scripts/eeg_preproc_deploy.py
@@ -10,7 +10,12 @@
 
 import argparse
 import time
+import sys
 from pathlib import Path
+
+# Add parent directory to path
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
 from utils import run_ssh, scp_to
 from config import CLUSTER_BASE, SLURM_ACCOUNT, LOCAL_BASE
 
